cefe0da6f24c38429ffe8bd98089e2bb8dcaaaeb
vattikotigit <154732593+vattikotigit@users.noreply.github.com>
2025-12-13 19:59:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+//require("dotenv").config();
 
 const app = express();
 app.use(express.json());
